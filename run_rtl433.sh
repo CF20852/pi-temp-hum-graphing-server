6491e4bec8db89e425006ac8e20822609d30f161
CF20852 <cfleming@ieee.org>
2017-08-12 08:42:31 -0400

    Updated to incorporate async file reads

--- run_rtl433.sh
+++ run_rtl433.sh
@@ -4,5 +4,5 @@
 # Because we only graph a point every 5 minutes, this is invoked by cron every 5 minutes
 # to avoid excessive writes to the temp.json file.
 #
-/usr/local/bin/rtl_433 -f 433960000 -p 60 -q -F json:/home/pi/rtl_433/temp.json -T 45
+/usr/local/bin/rtl_433 -f 433960000 -p 60 -q -F json:/home/pi/rtl_433/temp.json -T 60
 
--- server8.js
+++ server8.js
@@ -1,4 +1,4 @@
-// server4.js - NodeJS server for the PiThermServer project.
+// server7.js - NodeJS server for the PiThermServer project.
 
 /*
 
@@ -14,6 +14,7 @@ Ref: www.cl.cam.ac.uk/freshers/raspberrypi/tutorials/temperature/
 var fs = require('fs');
 var util = require('util');
 var http = require('http');
+var async = require('async');
 var sqlite3 = require('sqlite3');
 
 // Use node-static module to server chart for client-side dynamic graph
@@ -28,7 +29,6 @@ var db = new sqlite3.Database('./piTempHum.db');
 // Write a single temperature record in JSON format to database table.
 function insertTemp(data){
   // data is a javascript object
-  // the five ‘?’s are placeholders for date/time, attic temp, outside temp, outside dewpoint, and outside humidity
   var statement = db.prepare("INSERT INTO th_records VALUES (?, ?, ?, ?, ?)");
   // Insert values into prepared statement
   statement.run(data.th_record[0].unix_time, data.th_record[0].ds18b20, data.th_record[0].acuriteTemp, data.th_record[0].acuriteHum, data.th_record[0].acuriteDP);
@@ -41,64 +41,93 @@ function readTemp(callback){
   var d = new Date();
   var local_time = Date.now() - d.getTimezoneOffset()*60000;
   //console.log('Local time: ' + local_time);
-
-  var ds18b20Buf = fs.readFileSync('/sys/bus/w1/devices/28-000002afefb2/w1_slave');
-
-  // Read data from file (using fast node ASCII encoding).
-  var data = ds18b20Buf.toString('ascii').split(" "); // Split by space
-
-  // Extract temperature from string and divide by 1000 to give celsius
-  var tempDSC  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
-
-  // convert to Fahrenheit
-  tempDSF = tempDSC * 9 / 5 + 32;
-
-  // Round to one decimal place
-  tempDSF = Math.round(tempDSF * 10) / 10;
-  console.log('tempDSF = ' + tempDSF);
-
-  var acuriteBuf = fs.readFileSync('/home/pi/rtl_433/temp.json');
-
-  // Read data from file (using fast node ASCII encoding).
-  var data = acuriteBuf.toString('ascii').split(", "); // Split by comma+space
-
-  // Extract temperature from string
-  var tempArC  = parseFloat(data[data.length-2].split(" : ")[1]);
-
-  // convert to Fahrenheit
-  tempArF = tempArC * 9.0 / 5.0 + 32.0;
-
-  // Round to one decimal place
-  tempArF = Math.round(tempArF * 10) / 10;
-  console.log('tempArF = ' + tempArF);
-
-  // Extract humidity from string
-  var humAr  = parseFloat(data[data.length-1].split(" : ")[1]);
-  console.log('humAr = ' + humAr);
   
-  //calculate dewpoint using NWS algorithm
-  var esubs = 6.11 * Math.pow(10, 7.5 * tempArC / (237.3 + tempArC));
-  var dewPointC = 237.3 * Math.log(esubs * humAr / 611 ) / (7.5 * Math.log(10) - Math.log(esubs * humAr / 611));
-
-  // convert to Fahrenheit
-  var dewPointF = 9.0 / 5.0 * dewPointC + 32.0;
-
-  // round to one decimal place
-  var dewPointF = Math.round(dewPointF * 10) / 10;
+  var sensorFiles = ['/sys/bus/w1/devices/28-000002afefb2/w1_slave', '/home/pi/rtl_433/temp.json'];  
+  var buffer = [];  
   
-  // Add date/time to temperature & humidity values
-  var data = {
-    th_record:[{
-    unix_time: local_time,
-    ds18b20: tempDSF,
-    acuriteTemp: tempArF,
-    acuriteHum: humAr,
-    acuriteDP:  dewPointF
-  }]};
+  async.eachOfSeries(
+    // Pass items to iterate over
+    sensorFiles,
+    
+    // Pass iterator function that is called for each item
+    function(filename, key, cb) {
+      fs.readFile(filename, function(err, content) {
+        if (!err) {
+          buffer[key] = content;
+          //console.log('content = ' + content);
+        }
+        // Calling cb makes it go to the next item.
+        cb(err);
+      });
+    },
+    
+    // Final callback after each item has been iterated over.
+    function(err) {
+      if (err) {
+        console.log('An error occurred when reading sensor files.');
+      }
+      else {
+        var data = buffer[0].toString('ascii').split(" ");
+        if (data.length == 21) {
+          var tempDSC = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+        }
+        else {
+          var tempDSC = -20.0;
+          console.log('Error in DS18B20 file length!  Length = ' + data.length);
+        }
 
-  // Execute call back with data
-  callback(data);
+        // convert to Fahrenheit
+        tempDSF = tempDSC * 9 / 5 + 32;
 
+        // Round to one decimal place
+        tempDSF = Math.round(tempDSF * 10) / 10;
+        //console.log('tempDSF = ' + tempDSF);
+
+        var data = buffer[1].toString('ascii').split(", ");
+        if (data.length == 7) {
+          var tempArC  = parseFloat(data[data.length-2].split(" : ")[1]);
+          var humAr  = parseFloat(data[data.length-1].split(" : ")[1]);
+        }
+        else {
+          var tempArC = -20.0;
+          var humAr = 0.0;
+          //console.log('Error in Acurite file length!  Length = ' + data.length);
+        }
+        
+        // convert to Fahrenheit
+        tempArF = tempArC * 9.0 / 5.0 + 32.0;
+
+        // Round to one decimal place
+        tempArF = Math.round(tempArF * 10) / 10;
+        //console.log('tempArF = ' + tempArF);
+
+        // Calculate dewpoint using NWS algorithm
+        var esubs = 6.11 * Math.pow(10, 7.5 * tempArC / (237.3 + tempArC));
+        var dewPointC = 237.3 * Math.log(esubs * humAr / 611 ) / (7.5 * Math.log(10) - Math.log(esubs * humAr / 611));
+        
+        //convert to Fahrenheit
+        var dewPointF = 9.0 / 5.0 * dewPointC + 32.0;
+        
+        // round to one decimal place
+        dewPointF = Math.round(dewPointF * 10) / 10;
+        //console.log('dewPointF = ' + dewPointF);
+
+        // Add date/time to temperature & humidity values
+        var data = {
+          th_record:[{
+          unix_time: local_time,
+          ds18b20: tempDSF,
+          acuriteTemp: tempArF,
+          acuriteHum: humAr,
+          acuriteDP:  dewPointF
+        }]};
+
+        // Execute call back with data
+        callback(data);   
+      
+      }
+    }
+  );  
 };
 
 // Create a wrapper function which we'll use specifically for logging
@@ -209,6 +238,6 @@ logTemp(msecs);
 // Send a message to console
 console.log('Server is logging to database at '+msecs+'ms intervals');
 // Enable server
-server.listen(8087);
+server.listen(80);
 // Log message
-console.log('Server running at http://mr6225.no-ip.org:8087/temphum_logr4.htm');
+console.log('Server running at http://192.168.1.33:80');
